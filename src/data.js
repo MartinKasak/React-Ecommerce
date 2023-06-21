6ea5e49dd82f333af821f7ebc449b42b4269efcc
MartinKasak <martin.kasak11@gmail.com>
2023-06-22 00:10:26 +0300

    Working slider with data coming from data.js

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id:1, 
+        img:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.independent.co.uk%2F2021%2F07%2F02%2F16%2FiStock-1285065780.jpg%3Fwidth%3D982%26height%3D726%26auto%3Dwebp%26quality%3D75&f=1&nofb=1&ipt=7da03f38e988130e810c63c63d49ca1347233c06ea1ec7e48c4aee9cfd9ef5ff&ipo=images",
+        title:"Suvine Pakkumine",
+        desc:"Kõik tooted allahinnatud kuni 30%",
+        bg:"f5fafd",
+    },
+    {
+        id:2, 
+        img:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flifestylefrisco.s3.amazonaws.com%2Fwp-content%2Fuploads%2F2014%2F02%2FIBB-Gray-and-golden.jpg&f=1&nofb=1&ipt=e55f2a98b0185d58b2279ced7d4bb275e1f6c09f1f2151ab0c590c583491fb15&ipo=images",
+        title:"Sügisene Pakkumine",
+        desc:"Kõik tooted allahinnatud kuni 30%",
+        bg:"fcf2fd",
+    },
+    {
+        id:3, 
+        img:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Ftuyk0VQsxlxFWmd4upzBAhD2Rmk%3D%2F0x0%3A940x627%2F1200x0%2Ffilters%3Afocal(0x0%3A940x627)%3Ano_upscale()%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_asset%2Ffile%2F6812573%2Fonline-store-home-decor-home-goods-a-plus-r.0.jpg&f=1&nofb=1&ipt=80f9de93a7e349406159fad049776315d2153bd98098104575c18a64e0d3928e&ipo=images",
+        title:"Populaarsed Tooted",
+        desc:"Kõik tooted allahinnatud kuni 30%",
+        bg:"f7fadd",
+    },
+
+];
